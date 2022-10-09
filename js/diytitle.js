72f207f82da29323436d7abf6a4039b50ef903c0
CTTTTTTTT <1315719563@qq.com>
2022-10-09 21:00:32 +0800

    Site updated: 2022-10-09 21:00:31

--- js/diytitle.js
+++ js/diytitle.js
@@ -4,12 +4,12 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         //离开当前页面时标签显示内容
-        document.title = 'w(ﾟДﾟ)w';
+        document.title = '（o´・ェ・｀o）别走呀~';
         clearTimeout(titleTime);
     }
     else {
         //返回当前页面时标签显示内容
-        document.title = '♪(^∇^*)欢迎回来！' + OriginTitile;
+        document.title = '╰(*°▽°*)╯欢迎回来！' + OriginTitile;
         //两秒后变回正常标题
         titleTime = setTimeout(function () {
             document.title = OriginTitile;
